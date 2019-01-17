a4c9b571a7ca53a2b89d6157ecff90416b931f6c
Babiy-Alexander <Sancho_10_86@ukr.net>
2019-01-17 15:04:40 +0200

    home14 first commit

---
+++ home14/index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interactive Agency</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+                <img src="assets/img/Logo.png" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Offer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="header-box">
+            <div class="row justify-content-center">
+                <div class="col- header-content">
+                    <h1>Lorem ipsum dolor sit amet</h1>
+                    <span class="header-text">Nunc vel nibh tempor, pharetra lectus congue, luctus orci.</span>
+                    <a href="#" class="btn">Ask for price</a>
+                    <a href="#"><img class="scrool" src="assets/img/mouse.png" alt="mouse"></a>
+                    <span class="scrool-text">scrool down</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="best-design-code">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 best-design">
+                <h2>Best<span>Design</span></h2>
+                <p class="best-text">
+                    Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur ullamcorper
+                    auctor mauris, placerat fermentum lectus vulputate sed. Phasellus vestibulum sit amet dolor eget
+                    eleifend.
+                </p>
+                <a href="#" class="btn">Ask for price</a>
+            </div>
+            <div class="col-12 col-md-6 best-code">
+                <h2>Best<span>Code</span></h2>
+                <p class="best-text">
+                    Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur ullamcorper
+                    auctor mauris, placerat fermentum lectus vulputate sed. Phasellus vestibulum sit amet dolor eget
+                    eleifend.
+                </p>
+                <a href="#" class="btn">Ask for price</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="what-we-do">
+            <h2>What we do?</h2>
+            <div class="row">
+                <div class="col-12 col-lg-6 ">
+                    <div class="media">
+                        <img class="align-self-start mr-4" src="assets/img/icon-web-design.png"
+                             alt="icon-web-design">
+                        <div class="media-body">
+                            <h3 class="mt-0 subtitle-media-body">Web design</h3>
+                            <p class="text-media-body">
+                                Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                                Curabitur ullamcorper auctor mauris, placerat fermentum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 ">
+                    <div class="media">
+                        <img class="align-self-start mr-4" src="assets/img/icon-web-applications.png"
+                             alt="icon-web-applications">
+                        <div class="media-body">
+                            <h3 class="mt-0 subtitle-media-body">Web Applications</h3>
+                            <p class="text-media-body">
+                                Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                                Curabitur ullamcorper auctor mauris, placerat fermentum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-lg-6 ">
+                    <div class="media">
+                        <img class="align-self-start mr-4" src="assets/img/icon-digital-paiting.png"
+                             alt="digital-paiting">
+                        <div class="media-body">
+                            <h3 class="mt-0 subtitle-media-body">Digital paiting</h3>
+                            <p class="text-media-body">
+                                Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                                Curabitur ullamcorper auctor mauris, placerat fermentum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 ">
+                    <div class="media">
+                        <img class="align-self-start mr-4" src="assets/img/icon-desktop-applications.png"
+                             alt="icon-desktop-applications">
+                        <div class="media-body">
+                            <h3 class="mt-0 subtitle-media-body">Desktop Applications</h3>
+                            <p class="text-media-body">
+                                Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                                Curabitur ullamcorper auctor mauris, placerat fermentum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="btn">Ask for price</a>
+        </div>
+    </div>
+</section>
+<section class="slider">
+    <div class="row">
+        <div class="col-12 col-lg-4 drone-zone list">
+            <div class="text-hover">
+                <h3>Neptune template</h3>
+                <p class="slider-text">
+                    Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur ullamcorper
+                    auctor mauris, placerat fermentum.
+                </p>
+                <a href="#" class="btn">Watch more</a>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 profile list">
+            <div class="text-hover">
+                <h3>Neptune template</h3>
+                <p class="slider-text">
+                    Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur ullamcorper
+                    auctor mauris, placerat fermentum.
+                </p>
+                <a href="#" class="btn">Watch more</a>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 fit-app list">
+            <div class="text-hover">
+                <h3>Neptune template</h3>
+                <p class="slider-text">
+                    Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur ullamcorper
+                    auctor mauris, placerat fermentum.
+                </p>
+                <a href="#" class="btn">Watch more</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-8 m-auto">
+                <h2>About us!</h2>
+                <p class="text-about-us">
+                    Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                    Curabitur ullamcorper auctor mauris, placerat fermentum.
+                </p>
+            </div>
+        </div>
+        <div class="row main-items">
+            <div class="col-12 col-md-6 col-lg-3 items">
+                <img src="assets/img/participant-1.png" alt="participant-1">
+                <h4>John Doe</h4>
+                <p class="text-about-us">Curabitur ullamcorper auctor mauris, placerat fermentum.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 items">
+                <img src="assets/img/participant-2.png" alt="participant-2">
+                <h4>John Doe</h4>
+                <p class="text-about-us">Aliquam sagittis neque in lectus semper, arcu scelerisque.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 items">
+                <img src="assets/img/participant-3.png" alt="participant-3">
+                <h4>John Doe</h4>
+                <p class="text-about-us">Curabitur ullamcorper auctor mauris, placerat fermentum.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 items">
+                <img src="assets/img/participant-4.png" alt="participant-4">
+                <h4>John Doe</h4>
+                <p class="text-about-us">Neque in lectus semper, nec elementum arcu scelerisque.</p>
+            </div>
+        </div>
+        <a href="#" class="btn">Watch more</a>
+    </div>
+</section>
+<section class="maps">
+    <div style="overflow:hidden;width: 100%;height: 440px;"><iframe width="100%" height="610"
+         src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=Ukraine%2Ccherkasy+
+         (%D0%9D%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed"
+         frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><div><small>
+        <a href="https://embedgooglemaps.com/en/">embedgooglemaps EN</a></small></div><div><small>
+        <a href="https://iamsterdamcard.it">www.iamsterdamcard.it</a></small></div><style>#gmap_canvas
+        img{max-width:none!important;background:none!important}</style></div><br />
+</section>
+<script src="assets/js/libs.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
+$data = require('data.php');
